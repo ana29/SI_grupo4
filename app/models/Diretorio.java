ce04917aa7ef9e2513b45ab4a21fe45e8fc6a919
Marcos Cesar <marcos.cesar@ccc.ufcg.edu.br>
2016-08-05 17:56:07 -0300

    Objeto Diretório

---
+++ app/models/Diretorio.java
@@ -0,0 +1,43 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by marco on 02/08/2016.
+ */
+public class Diretorio {
+    private String nome;
+    private List<Diretorio> subDiretorios;
+    private List<Diretorio> arquivos;
+
+    public Diretorio(String nome){
+        this.nome = nome;
+        this.subDiretorios = new ArrayList<>();
+        this.arquivos = new ArrayList<>();
+    }
+
+
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (o instanceof Diretorio){
+            Diretorio obj = (Diretorio) o;
+            return obj.getNome().equals(this.getNome());
+        }
+        return false;
+    }
+
+    public List<Diretorio> getSubDiretorios(){
+        return this.subDiretorios;
+    }
+
+    public List<Diretorio> getArquivos() {
+        return arquivos;
+    }
+
+}
--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -5,17 +5,44 @@ public class Usuario {
     public String nome;
     public String email;
     public String senha;
+    public Diretorio pastaPessoal;
 
     public Usuario(){
 
     }
 
     public Usuario(String nome, String email, String senha){
-
+        this.pastaPessoal = new Diretorio("root");
         this.nome = nome;
         this.email = email;
         this.senha = senha;
 
+    }
+
+    public void criaSubDiretorio(String nome){
+        if (!pastaPessoal.getSubDiretorios().contains(nome)){
+            pastaPessoal.getSubDiretorios().add(new Diretorio(nome));
+        }
+        else{
+            boolean adicionado = false;
+            int count = 0;
+            while (adicionado == false){
+                String novoNome = nome + "(" + count + ")";
+                if (!pastaPessoal.getSubDiretorios().contains(novoNome)){
+                    pastaPessoal.getSubDiretorios().add(new Diretorio(novoNome));
+                    adicionado = true;
+                }
+                count ++;
+            }
+        }
+    }
+
+    public void excluirSubDiretorio(String nome){
+        for (Diretorio d: pastaPessoal.getSubDiretorios()) {
+            if (d.getNome().equals(nome)){
+                pastaPessoal.getSubDiretorios().remove(d);
+            }
+        }
     }
 
     public String getNome() {
@@ -40,6 +67,10 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Diretorio getPastaPessoal() {
+        return pastaPessoal;
     }
 }
 
